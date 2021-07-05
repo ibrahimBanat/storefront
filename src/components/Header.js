12836f59ef9934ee87fe80cc46a5c6015dda76e1
Ibrahim Banat <ibrahim.banat.97@gmail.com>
2021-07-05 22:00:33 +0300

    modyfying the cart to be as an icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,6 +7,19 @@ import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
 import Toolbar from '@material-ui/core/Toolbar';
 import { showCart } from '../provider/actions/showCart';
+import Badge from '@material-ui/core/Badge';
+import { withStyles } from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+
+const StyledBadge = withStyles(theme => ({
+  badge: {
+    right: -3,
+    top: 13,
+    border: `2px solid ${theme.palette.background.paper}`,
+    padding: '0 4px',
+  },
+}))(Badge);
 
 const useStyles = makeStyles(theme => ({
   space: {
@@ -19,7 +32,11 @@ function Header(props) {
     <AppBar color='' position='static' style={{ backgroundColor: '#FB2E86' }}>
       <Toolbar className={style.space}>
         <Button>OUR STORE</Button>
-        <Button>Cart({props.cart.cart.length})</Button>
+        <IconButton aria-label='cart'>
+          <StyledBadge badgeContent={props.cart.cart.length} color='secondary'>
+            <ShoppingCartIcon />
+          </StyledBadge>
+        </IconButton>
       </Toolbar>
     </AppBar>
   );
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { connect } from 'react-redux';
-
 import { addProduct } from '../provider/actions/addProduct';
 import ProductCard from './ProductCard';
 import GridList from '@material-ui/core/GridList';
