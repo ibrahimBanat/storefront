1088ebfdf7c62ba0a91930226fe19efdef7636d2
Ibrahim Banat <ibrahim.banat.97@gmail.com>
2021-07-05 22:50:14 +0300

    adding the cart functionality

--- src/components/SimpleCart.js
+++ src/components/SimpleCart.js
@@ -3,7 +3,8 @@ import { makeStyles } from '@material-ui/core/styles';
 import Chip from '@material-ui/core/Chip';
 import Paper from '@material-ui/core/Paper';
 import TagFacesIcon from '@material-ui/icons/TagFaces';
-
+import { connect } from 'react-redux';
+import { If, Else } from 'react-if';
 const useStyles = makeStyles(theme => ({
   root: {
     display: 'flex',
@@ -18,7 +19,7 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
-export default function ChipsArray() {
+const ChipsArray = props => {
   const classes = useStyles();
   const [chipData, setChipData] = React.useState([
     { key: 0, label: 'Angular' },
@@ -28,26 +29,36 @@ export default function ChipsArray() {
     { key: 4, label: 'Vue.js' },
   ]);
 
-  const handleDelete = chipToDelete => () => {
+  const handleDelete = chipToDelete => props => {
     setChipData(chips => chips.filter(chip => chip.key !== chipToDelete.key));
   };
-
+  console.log(props);
   return (
-    <Paper component='ul' className={classes.root}>
-      {chipData.map(data => {
-        let icon;
-
-        return (
-          <li key={data.key}>
-            <Chip
-              icon={icon}
-              label={data.label}
-              onDelete={data.label === 'React' ? undefined : handleDelete(data)}
-              className={classes.chip}
-            />
-          </li>
-        );
-      })}
-    </Paper>
+    <>
+      <If condition={() => props.cart.length !== 0}>
+        <Paper component='ul' className={classes.root}>
+          {props.cart.map((item, idx) => {
+            console.log(item);
+            return (
+              <li key={idx}>
+                <Chip
+                  label={item.name}
+                  onDelete={
+                    item.label === 'React' ? undefined : handleDelete(item)
+                  }
+                  className={classes.chip}
+                />
+              </li>
+            );
+          })}
+        </Paper>
+      </If>
+      <Else>{}</Else>
+    </>
   );
-}
+};
+const mapStateToProps = state => {
+  return { cart: state.cart.cart };
+};
+
+export default connect(mapStateToProps)(ChipsArray);
